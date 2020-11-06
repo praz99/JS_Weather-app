be66d9d9b4f9dbd34b83f7c9f76c569e1c7fa579
praz <t.prazwal@gmail.com>
2020-11-06 15:09:01 +0100

    display weather information

--- src/logic.js
+++ src/logic.js
@@ -2,7 +2,7 @@ const newForm = document.querySelector('[data-search-city-form]');
 const cityInput = document.querySelector('[data-city-name-input]');
 
 const getWeather = async () => {
-  const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Kathmandu&APPID=108f7da063795b7306e8b024f6068b48', {mode: 'cors'});
+  const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Bydgoszcz&APPID=108f7da063795b7306e8b024f6068b48', {mode: 'cors'});
   const getData = await response.json();
   console.log(getData);
 }
