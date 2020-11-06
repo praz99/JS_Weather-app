b6e4bc314c16d226129e09a995d217ef2f0fc034
praz <t.prazwal@gmail.com>
2020-11-06 14:22:51 +0100

    add input field and basic sytling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-const getWeather = async () => {
-  const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Kathmandu&APPID=108f7da063795b7306e8b024f6068b48', {mode: 'cors'});
-  const getData = await response.json();
-  console.log(getData);
-}
+import './style.css';
+import Weather from './logic';
 
-getWeather();
+Weather();
