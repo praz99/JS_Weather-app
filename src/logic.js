f42bbf6aaae5bc719e1a537464af97041e776705
praz <t.prazwal@gmail.com>
2020-11-06 17:55:16 +0100

    fix issue of not showing data after invalid search

--- src/logic.js
+++ src/logic.js
@@ -1,6 +1,3 @@
-import { formatters } from "stylelint";
-
-const weatherContainer = document.querySelector('[data-display-weather-container]');
 const showCity = document.querySelector('[data-show-city-country]');
 const showWeatherMain = document.querySelector('[data-show-weather-main]');
 const showWeatherDesc = document.querySelector('[data-show-weather-desc]');
@@ -14,25 +11,27 @@ const api_key = '108f7da063795b7306e8b024f6068b48';
 
 const getWeather = async(city) => {
   try {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${api_key}`, {mode: 'cors'});
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${api_key}&units=metric`, {mode: 'cors'});
     const getData = await response.json();
-    showCity.innerText = `${getData.name}, ${getData.sys.country}`;
+    showCity.innerText = `${ getData.name }, ${ getData.sys.country }`;
     showWeatherMain.innerText = getData.weather[0].main;
     showWeatherDesc.innerText = getData.weather[0].description;
-    showTempMain.innerText = toCelcius(getData.main.temp);
-    showTempFeel.innerText = toCelcius(getData.main.feels_like);
-    showTempMax.innerText = toCelcius(getData.main.temp_max);
-    showTempMin.innerText = toCelcius(getData.main.temp_min);
+    showTempMain.innerText = getData.main.temp;
+    showTempFeel.innerText = getData.main.feels_like;
+    showTempMax.innerText = getData.main.temp_max;
+    showTempMin.innerText = getData.main.temp_min;
     showWind.innerText = getData.wind.speed;
     console.log(getData);
   } catch(err) {
-    weatherContainer.innerText = `No data available for ${city}.`
+    showCity.innerText = `No data available for ${city}`;
+    showWeatherMain.innerText = '';
+    showWeatherDesc.innerText = '';
+    showTempMain.innerText = '';
+    showTempFeel.innerText = '';
+    showTempMax.innerText = '';
+    showTempMin.innerText = '';
+    showWind.innerText = '';
   }
-}
-
-const toCelcius = (kelvin) => {
-  const celcius = kelvin - 273;
-  return Math.round(celcius);
 }
 
 export default getWeather;
